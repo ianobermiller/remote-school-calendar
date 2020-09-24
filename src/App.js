443a60c5a7f58a37827b314e8939bbc27fa5529e
Ian Obermiller <ian@obermillers.com>
2020-09-24 10:36:09 -0700

    Extra data and components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React, {useEffect, useState} from 'react';
-import {Temporal} from 'proposal-temporal';
 import {css} from 'emotion';
+import {Temporal} from 'proposal-temporal';
+import React, {useEffect, useState} from 'react';
+import {CALENDAR_DATA} from './CalendarData';
 import createSilentAudio from './createSilentAudio';
 
 const audioSrc = createSilentAudio(10);
@@ -9,237 +10,7 @@ const START_TIME = Temporal.Time.from({hour: 8, minute: 30});
 const END_TIME = Temporal.Time.from({hour: 14, minute: 30});
 const LABEL_STEP = Temporal.Duration.from({minutes: 30});
 
-const CALENDARS_RAW = [
-  {
-    name: 'Adaira',
-    color: 'rgb(220 130 18)',
-    eventsByDay: [
-      // Monday
-      [
-        {title: 'Class Meeting', start: '08:30'},
-        {title: 'Art / PE / Music', start: '09:00'},
-        {title: 'Reading - Seesaw', start: '09:30'},
-        {title: 'ELA Group 2', start: '10:00'},
-        {title: 'Break', start: '10:30'},
-        {title: 'Math - Seesaw', start: '11:00'},
-        {title: 'Lunch', start: '11:30'},
-        {title: 'Math - Group 2', start: '12:15'},
-        {title: 'Break', start: '12:45'},
-        {title: 'Science / Social Studies', start: '13:00'},
-      ],
-      // Tuesday
-      [
-        {title: 'Class Meeting', start: '08:30'},
-        {title: 'Art / PE / Music', start: '09:00'},
-        {title: 'Reading - Seesaw', start: '09:30'},
-        {title: 'ELA Group 2', start: '10:00'},
-        {title: 'Break', start: '10:30'},
-        {title: 'Math - Seesaw', start: '11:00'},
-        {title: 'Lunch', start: '11:30'},
-        {title: 'Math - Group 2', start: '12:15'},
-        {title: 'Break', start: '12:45'},
-        {title: 'Science / Social Studies', start: '13:00'},
-      ],
-      // Wednesday
-      [
-        {title: 'Class Meeting', start: '08:30'},
-        {title: 'Art / PE / Music', start: '09:00'},
-        {title: 'Reading - Live', start: '09:30'},
-        {title: 'Break', start: '10:00'},
-        {title: 'Math', start: '10:15'},
-        {title: 'Break', start: '10:45'},
-        {title: 'Science / Social Studies', start: '11:00'},
-        {title: 'Lunch', start: '11:30', end: '12:15'},
-      ],
-      // Thursday
-      [
-        {title: 'Class Meeting', start: '08:30'},
-        {title: 'Art / PE / Music', start: '09:00'},
-        {title: 'Reading - Seesaw', start: '09:30'},
-        {title: 'ELA Group 2', start: '10:00'},
-        {title: 'Break', start: '10:30'},
-        {title: 'Math - Seesaw', start: '11:00'},
-        {title: 'Lunch', start: '11:30'},
-        {title: 'Math - Group 2', start: '12:15'},
-        {title: 'Break', start: '12:45'},
-        {title: 'Science / Social Studies', start: '13:00'},
-      ],
-      // Friday
-      [
-        {title: 'Class Meeting', start: '08:30'},
-        {title: 'Art / PE / Music', start: '09:00'},
-        {title: 'Reading - Seesaw', start: '09:30'},
-        {title: 'ELA Group 2', start: '10:00'},
-        {title: 'Break', start: '10:30'},
-        {title: 'Math - Seesaw', start: '11:00'},
-        {title: 'Lunch', start: '11:30'},
-        {title: 'Math - Group 2', start: '12:15'},
-        {title: 'Break', start: '12:45'},
-        {title: 'Science / Social Studies', start: '13:00'},
-      ],
-    ],
-  },
-  {
-    name: 'Isla',
-    color: 'rgb(181 63 172)',
-    eventsByDay: [
-      // Monday
-      [
-        {title: 'Welcome / SEL', start: '08:30'},
-        {title: 'Writing', start: '09:00'},
-        {title: 'Math', start: '09:45'},
-        {title: 'Break', start: '10:30'},
-        {title: 'PE - Live', start: '11:00'},
-        {title: 'Reading', start: '11:30'},
-        {title: 'Lunch', start: '12:00'},
-        {title: 'Science / Social Studies', start: '12:45', end: '13:15'},
-        // {
-        //   title: 'Independent Work / Small Groups',
-        //   start: '13:20',
-        //   end: '14:30',
-        // },
-      ],
-      // Tuesday
-      [
-        {title: 'Welcome / SEL', start: '08:30'},
-        {title: 'Writing', start: '09:00'},
-        {title: 'Math', start: '09:45'},
-        {title: 'Break', start: '10:30'},
-        {title: 'PE - Seesaw', start: '11:00'},
-        {title: 'Reading', start: '11:30'},
-        {title: 'Lunch', start: '12:00'},
-        {title: 'Science / Social Studies', start: '12:45', end: '13:15'},
-        {
-          title: 'Reading',
-          start: '13:20',
-          end: '13:50',
-        },
-        // {
-        //   title: 'Independent Work / Small Groups',
-        //   start: '13:55',
-        //   end: '14:30',
-        // },
-      ],
-      // Wednesday
-      [
-        {title: 'Welcome / SEL', start: '08:30'},
-        {title: 'Writing', start: '09:00'},
-        {title: 'Reading', start: '09:30'},
-        {title: 'Independent Work / Small Groups', start: '10:00'},
-        {title: 'Break', start: '10:30'},
-        {title: 'PE - Seesaw', start: '11:00'},
-        {title: 'Reading', start: '11:30'},
-        {title: 'Lunch', start: '12:00', end: '12:45'},
-        // {title: 'Community Meeting', start: '12:45', end: '13:15'},
-        // {
-        //   title: 'Independent Work / Small Groups',
-        //   start: '13:15',
-        //   end: '13:45',
-        // },
-      ],
-      // Thursday
-      [
-        {title: 'Welcome / SEL', start: '08:30'},
-        {title: 'Writing', start: '09:00'},
-        {title: 'Math', start: '09:45'},
-        {title: 'Break', start: '10:30'},
-        {title: 'PE - Seesaw', start: '11:00'},
-        {title: 'Reading', start: '11:30'},
-        {title: 'Lunch', start: '12:00'},
-        {title: 'Science / Social Studies', start: '12:45', end: '13:15'},
-        {
-          title: 'Reading',
-          start: '13:20',
-          end: '13:50',
-        },
-        // {
-        //   title: 'Independent Work / Small Groups',
-        //   start: '13:55',
-        //   end: '14:30',
-        // },
-      ],
-      // Friday
-      [
-        {title: 'Welcome / SEL', start: '08:30'},
-        {title: 'Writing', start: '09:00'},
-        {title: 'Math', start: '09:45'},
-        {title: 'Break', start: '10:30'},
-        {title: 'PE - Seesaw', start: '11:00'},
-        {title: 'Reading', start: '11:30'},
-        {title: 'Lunch', start: '12:00'},
-        {title: 'Science / Social Studies', start: '12:45', end: '13:15'},
-        // {
-        //   title: 'Independent Work / Small Groups',
-        //   start: '13:20',
-        //   end: '14:30',
-        // },
-      ],
-    ],
-  },
-  {
-    name: 'Wellington',
-    color: 'rgb(60 165 78)',
-    eventsByDay: [
-      // Monday
-      [
-        {title: 'Homeroom / Math / Science', start: '08:30'},
-        {title: 'PE - Seesaw', start: '10:00'},
-        {title: 'Break', start: '10:30'},
-        {title: 'ELA', start: '11:00'},
-        {title: 'Lunch', start: '12:00'},
-        {title: 'Break', start: '12:45'},
-        {title: 'Art or Music', start: '13:20', end: '13:50'},
-        {title: 'Science', start: '14:00', end: '14:30'},
-      ],
-      // Tuesday
-      [
-        {title: 'Homeroom / Math / Science', start: '08:30'},
-        {title: 'PE - Live', start: '10:00'},
-        {title: 'Break', start: '10:30'},
-        {title: 'ELA', start: '11:00'},
-        {title: 'Lunch', start: '12:00'},
-        {title: 'Break', start: '12:45'},
-        {title: 'Small Groups', start: '13:10'},
-        {title: 'Break', start: '13:30'},
-        {title: 'Science', start: '14:00', end: '14:30'},
-      ],
-      // Wednesday
-      [
-        {title: 'Homeroom / Math / Science', start: '08:30'},
-        {title: 'PE - Seesaw', start: '10:00'},
-        {title: 'Break', start: '10:30'},
-        {title: 'ELA', start: '11:00'},
-        {title: 'Lunch', start: '12:00', end: '12:45'},
-        // {title: 'Community Meeting', start: '12:45', end: '13:15'},
-      ],
-      // Thursday
-      [
-        {title: 'Homeroom / Math / Science', start: '08:30'},
-        {title: 'PE - Seesaw', start: '10:00'},
-        {title: 'Break', start: '10:30'},
-        {title: 'ELA', start: '11:00'},
-        {title: 'Lunch', start: '12:00'},
-        {title: 'Break', start: '12:45'},
-        {title: 'Art or Music', start: '13:20', end: '13:50'},
-        {title: 'Social Studies', start: '14:00', end: '14:30'},
-      ],
-      // Friday
-      [
-        {title: 'Homeroom / Math / Science', start: '08:30'},
-        {title: 'PE - Seesaw', start: '10:00'},
-        {title: 'Break', start: '10:30'},
-        {title: 'ELA', start: '11:00'},
-        {title: 'Lunch', start: '12:00'},
-        {title: 'Break', start: '12:45'},
-        {title: 'Social Studies', start: '13:10'},
-        {title: 'Break', start: '13:30'},
-        {title: 'Social Studies', start: '14:00', end: '14:30'},
-      ],
-    ],
-  },
-];
-
-const CALENDARS = CALENDARS_RAW.map(cal => ({
+const CALENDARS = CALENDAR_DATA.map(cal => ({
   ...cal,
   eventsByDay: cal.eventsByDay.map(events =>
     events.map((ev, i) => {
@@ -303,100 +74,32 @@ function App() {
         grid-auto-rows: minmax(0, 1fr);
         grid-auto-columns: minmax(0, 1fr);
       `}>
-      {rows.map((t, i) => [
-        <div
-          className={css`
-            color: #666;
-            font-size: 80%;
-            padding-right: 8px;
-            text-align: right;
-            transform: translateY(-6px);
-          `}
-          key={`label-${t.toString()}`}
-          style={{
-            gridColumn: 1,
-            gridRow: toGridRow(t),
-          }}>
-          {timeFormatter.format(t)}
-        </div>,
-        <div
-          className={css`
-            border-top: solid 1px #ccc;
-          `}
-          key={`divider-${t.toString()}`}
-          style={{
-            gridColumnStart: 2,
-            gridColumnEnd: CALENDARS.length + 2,
-            gridRow: toGridRow(t),
-          }}
-        />,
-      ])}
+      {rows.map((t, i) => (
+        <TimeRow time={t} />
+      ))}
 
-      {CALENDARS.map((cal, calIndex) => [
-        <h1
-          className={css`
-            font-size: 120%;
-            margin: 0 0 16px 8px;
-          `}
+      {CALENDARS.map((cal, calIndex) => (
+        <CalendarHeader
+          calendar={cal}
+          index={calIndex}
           key={`calendar-${calIndex}-label`}
-          style={{
-            gridColumn: calIndex + 2,
-            gridRow: 1,
-          }}>
-          {cal.name}
-        </h1>,
-        (cal.eventsByDay[Temporal.now.date().dayOfWeek - 1] ?? []).map(
-          (ev, evIndex) => {
-            let background = ev.color || cal.color || 'rgb(63, 81, 181)';
-            return (
-              <div
-                className={css`
-                  border-radius: 4px;
-                  color: white;
-                  padding: 4px;
-                  margin-bottom: 2px;
-                  margin-left: 8px;
-                `}
-                key={`calendar-${calIndex}_event-${evIndex}`}
-                style={{
-                  background,
-                  gridColumn: calIndex + 2,
-                  gridRowStart: toGridRow(ev.start),
-                  gridRowEnd: toGridRow(ev.end),
-                  opacity: ev.opacity,
-                }}>
-                {ev.title}
-              </div>
-            );
-          },
-        ),
-      ])}
+        />
+      ))}
 
-      {Temporal.Time.compare(currentTime, START_TIME) >= 0 &&
-        Temporal.Time.compare(currentTime, END_TIME) <= 0 && (
-          <div
-            className={css`
-              border-top: solid 4px red;
-              position: relative;
-            `}
-            style={{
-              gridColumnStart: 1,
-              gridColumnEnd: CALENDARS.length + 2,
-              gridRow: toGridRow(currentTime),
-            }}>
-            <div
-              className={css`
-                color: red;
-                font-weight: bold;
-                position: absolute;
-                right: calc(100% + 4px);
-                top: -16px;
-                white-space: nowrap;
-              `}>
-              {timeFormatter.format(currentTime)}
-            </div>
-          </div>
-        )}
+      {CALENDARS.map((cal, calIndex) =>
+        (
+          cal.eventsByDay[Temporal.now.date().dayOfWeek - 1] ?? []
+        ).map((ev, evIndex) => (
+          <CalendarEvent
+            calendar={cal}
+            calendarIndex={calIndex}
+            event={ev}
+            key={`calendar-${calIndex}_event-${evIndex}`}
+          />
+        )),
+      )}
+
+      <CurrentTimeIndicator currentTime={currentTime} />
 
       {isPlayingAudio && (
         <audio autoPlay={true} controls={false} loop={true} src={audioSrc} />
@@ -437,6 +140,110 @@ function App() {
           </button>
         </div>
       )}
+    </div>
+  );
+}
+
+function TimeRow({time}) {
+  return [
+    <div
+      className={css`
+        color: #666;
+        font-size: 80%;
+        padding-right: 8px;
+        text-align: right;
+        transform: translateY(-6px);
+      `}
+      key={`label-${time.toString()}`}
+      style={{
+        gridColumn: 1,
+        gridRow: toGridRow(time),
+      }}>
+      {timeFormatter.format(time)}
+    </div>,
+    <div
+      className={css`
+        border-top: solid 1px #ccc;
+      `}
+      key={`divider-${time.toString()}`}
+      style={{
+        gridColumnStart: 2,
+        gridColumnEnd: CALENDARS.length + 2,
+        gridRow: toGridRow(time),
+      }}
+    />,
+  ];
+}
+
+function CalendarEvent({event, calendar, calendarIndex}) {
+  let background = event.color || calendar.color || 'rgb(63, 81, 181)';
+  return (
+    <div
+      className={css`
+        border-radius: 4px;
+        color: white;
+        padding: 4px;
+        margin-bottom: 2px;
+        margin-left: 8px;
+      `}
+      style={{
+        background,
+        gridColumn: calendarIndex + 2,
+        gridRowStart: toGridRow(event.start),
+        gridRowEnd: toGridRow(event.end),
+        opacity: event.opacity,
+      }}>
+      {event.title}
+    </div>
+  );
+}
+
+function CalendarHeader({index, calendar}) {
+  return (
+    <h1
+      className={css`
+        font-size: 120%;
+        margin: 0 0 16px 8px;
+      `}
+      style={{
+        gridColumn: index + 2,
+        gridRow: 1,
+      }}>
+      {calendar.name}
+    </h1>
+  );
+}
+
+function CurrentTimeIndicator({currentTime}) {
+  if (
+    Temporal.Time.compare(currentTime, START_TIME) < 0 ||
+    Temporal.Time.compare(currentTime, END_TIME) > 0
+  ) {
+    return null;
+  }
+
+  return (
+    <div
+      className={css`
+        border-top: solid 4px red;
+        position: relative;
+      `}
+      style={{
+        gridColumnStart: 1,
+        gridColumnEnd: CALENDARS.length + 2,
+        gridRow: toGridRow(currentTime),
+      }}>
+      <div
+        className={css`
+          color: red;
+          font-weight: bold;
+          position: absolute;
+          right: calc(100% + 4px);
+          top: -16px;
+          white-space: nowrap;
+        `}>
+        {timeFormatter.format(currentTime)}
+      </div>
     </div>
   );
 }
